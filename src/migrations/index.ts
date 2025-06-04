9f98185d8f171c6874c8ba7f273fd61dfa0beaea
yuvalkonimbo <yuval@konimbo.co.il>
2025-06-04 17:31:41 +0300

    Add base Migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250604_105247 from './20250604_105247.js'
+
+export const migrations = [
+  {
+    up: migration_20250604_105247.up,
+    down: migration_20250604_105247.down,
+    name: '20250604_105247',
+  },
+]
