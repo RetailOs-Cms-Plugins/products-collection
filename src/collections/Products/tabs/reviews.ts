a86377bf63567ad001c50cf7d45148c8151de89b
yuvalkonimbo <yuval@konimbo.co.il>
2025-06-08 13:05:38 +0300

    Add products collection's files

---
+++ src/collections/Products/tabs/reviews.ts
@@ -0,0 +1,33 @@
+import { Tab } from 'payload'
+
+export const reviewsTab: Tab = {
+  label: 'Reviews',
+  fields: [
+    {
+      name: 'reviews',
+      type: 'array',
+      fields: [
+        {
+          type: 'row',
+          fields: [
+            { name: 'customer', type: 'text' },
+            { name: 'date', type: 'date' },
+          ],
+        },
+        {
+          name: 'rating',
+          type: 'radio',
+          options: Array.from({ length: 5 }, (_, i) => ({
+            label: `${i + 1}`,
+            value: (i + 1).toString(),
+          })),
+          hooks: {
+            beforeChange: [({ value }) => Number(value)], // save to DB as num and not string
+          },
+        },
+        { name: 'review', type: 'richText' },
+        { name: 'image', type: 'upload', relationTo: 'media' },
+      ],
+    },
+  ],
+}
